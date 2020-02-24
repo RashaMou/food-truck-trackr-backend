9682edd1cfce5402a220f648943c79baf0407d30
Rasha Moumneh <rasha@fastmail.com>
2020-02-23 23:12:09 -0500

    add user-truck-rating migration

--- database/migrations/20200223230940_user-rating-table.js
+++ database/migrations/20200223230940_user-rating-table.js
@@ -1,25 +1,26 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('favorites', tbl => {
+  return knex.schema.createTable('user-truck-rating', tbl => {
     tbl.increments();
+    tbl.decimal('rating', 1, 2).notNullable();
     tbl
-      .integer('users_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('users')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
-    tbl
-      .integer('trucks_id')
+      .integer('truck_id')
       .unsigned()
       .notNullable()
       .references('id')
       .inTable('trucks')
       .onDelete('CASCADE')
       .onUpdate('CASCADE');
+    tbl
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('favorites');
+  return knex.schema.dropTableIfExists('user-truck-rating');
 };
