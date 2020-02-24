08893d08e4ef7b266c06198dcc6c94a2addb1c6e
Rasha Moumneh <rasha@fastmail.com>
2020-02-23 19:48:49 -0500

    add server.js

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/foodtrucktrackr.db'
+      filename: './database/foodtrucktrackr.db'
     },
     useNullAsDefault: true,
     migrations: {
