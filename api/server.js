a07c1ce671edb232ed4e1c54288c899e2fa3d333
Rasha Moumneh <rasha@fastmail.com>
2020-02-23 22:16:55 -0500

    add users router

--- api/server.js
+++ api/server.js
@@ -5,11 +5,13 @@ const helmet = require('helmet');
 const server = express();
 
 const authRouter = require('../api/auth/auth-router');
+const usersRouter = require('../api/users/users-router');
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send('welcome to foodtruck trackr');
