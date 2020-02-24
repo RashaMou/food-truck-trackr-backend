b6af52e12c7ff9e1cb2947584dafcc5220d803b1
Rasha Moumneh <rasha@fastmail.com>
2020-02-23 20:48:33 -0500

    add users migration

--- database/migrations/20200223192324_users-table.js
+++ database/migrations/20200223192324_users-table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl
+      .string('email', 128)
+      .notNullable()
+      .unique();
+    tbl.string('password', 128).notNullable();
+    tbl.string('name', 128);
+    tbl.string('avatar', 128);
+    tbl.string('role', 8);
+  });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
