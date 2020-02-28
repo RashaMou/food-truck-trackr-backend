39f26e50287d61526221f9a94c6fed7882956b7a
Rasha Moumneh <rasha@fastmail.com>
2020-02-28 00:44:58 -0500

    Add console.log(error) to register request

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -20,14 +20,18 @@ function findBy(filter) {
 }
 
 function findById(id) {
-  return db('users')
-    .select('id', 'username', 'email', 'role', 'avatar')
-    .where({ id })
-    .first();
+  return (
+    db('users')
+      // .select('id', 'username', 'email', 'role', 'avatar')
+      .where({ id })
+      .first()
+  );
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user, 'id');
+  const [id] = await db('users')
+    .insert(user)
+    .returning('id');
   return findById(id);
 }
 
