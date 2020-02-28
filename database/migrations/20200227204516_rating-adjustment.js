052828d2b0dcf590eb6319d81a2a2feba00d761e
Rasha Moumneh <rasha@fastmail.com>
2020-02-27 22:29:46 -0500

    Rolled back migrations and redid user-ratings migration

--- database/migrations/20200227204516_rating-adjustment.js
+++
@@ -1,8 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.table('user-truck-rating', tbl => {
-    tbl.dropColumn('rating');
-    tbl.decimal('user-rating', 2, 1);
-  });
-};
-
-exports.down = function(knex) {};
--- database/migrations/20200227215453_rating-fix.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.table('user-truck-rating', tbl => {
-    tbl.decimal('user-rating', 2, 1);
-  });
-};
-
-exports.down = function(knex) {};
